ad24af497692bed5ee5145316c6799c9045f0c02
tutengfei <tutengfei.kevin@gmail.com>
2016-09-20 22:08:27 +0800

    添加作者标签

--- beeblog/views.py
+++ beeblog/views.py
@@ -5,18 +5,21 @@ from django.shortcuts import render
 from django.views import generic
 
 from .config import PER_PAGE
-from .models import Category, Article
+from .models import Article, Author, Category
 
 
 def my_render(request, template, kwargs):
     if 'categories' not in kwargs:
         kwargs['categories'] = Category.query_objects.all()
+    if 'authors' not in kwargs:
+        kwargs['authors'] = Author.query_objects.all()
     return render(request, template, kwargs)
 
 
 class BaseDetailView(generic.DetailView):
     def render_to_response(self, context, **response_kwargs):
         context['categories'] = Category.query_objects.all()
+        context['authors'] = Author.query_objects.all()
         return super().render_to_response(context, **response_kwargs)
 
 
@@ -50,6 +53,24 @@ def list_recent_articles_view(request, days):
 
 def list_category_article_view(request, category_id):
     articles = Article.query_objects.filter(categories__id=category_id).all()
+    paginator = Paginator(articles, PER_PAGE)  # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        articles = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        articles = paginator.page(paginator.num_pages)
+    return my_render(request, 'beeblog/list.html', {
+        'page_objects': articles
+    })
+
+
+def list_author_article_view(request, author_id):
+    articles = Article.query_objects.filter(author__id=author_id).all()
     paginator = Paginator(articles, PER_PAGE)  # Show 25 contacts per page
 
     page = request.GET.get('page')
